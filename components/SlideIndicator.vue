f3f0605667d96072cb51b66934567dcf9b12ef06
Tolu ISOLA <i_tolu@yahoo.com>
2023-11-13 10:23:51 +0100

    :wrench: Chore:  Mobile 3 steps and Web 4 steps card animation

---
+++ components/SlideIndicator.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="indicator h-2 rounded-md cursor-pointer" :class="active ? 'w-8 bg-['+activeColor+']' : 'w-2 bg-['+defaultColor+']'"></div>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        active: boolean,
+        defaultColor: string,
+        activeColor: string
+    }>()
+</script>
