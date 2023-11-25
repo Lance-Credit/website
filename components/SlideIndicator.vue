1ee6d7edcca17ff59f89a2ac8c83ee8017ed8564
Tolu ISOLA <i_tolu@yahoo.com>
2023-11-25 12:03:18 +0100

    :bug: Fix: Show state of testimonials

--- components/SlideIndicator.vue
+++ components/SlideIndicator.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="indicator h-2 rounded-md cursor-pointer" :class="active ? 'w-8 bg-['+activeColor+']' : 'w-2 bg-['+defaultColor+']'"></div>
+    <div class="indicator h-2 rounded-md cursor-pointer" :class="active ? 'w-8' : 'w-2'" :style="active ? `background: ${activeColor}` : `background: ${defaultColor}`"></div>
 </template>
 
 <script setup lang="ts">
